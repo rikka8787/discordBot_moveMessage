3f0ebfe885ebe969a0b1227b73d00b06378a5456
rikka8787 <135810136+rikka8787@users.noreply.github.com>
2024-04-09 23:51:07 +0800

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -3,8 +3,4 @@ var http = require('http')
 http.createServer(function (req, res) {
   res.write("I'm alive");
   res.end();
-});
-
-server.listen(8080, function() {
-  console.log('Server is running on http://localhost:8080');
-});
+}).listen(8080);
